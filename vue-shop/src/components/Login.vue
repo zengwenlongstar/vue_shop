34004f671557b47058f83038d627e6e1516611d3
zengwenlongstar <1658451483@qq.com>
2020-11-28 08:35:09 +0800

    第三此提交

--- vue-shop/src/components/Login.vue
+++ vue-shop/src/components/Login.vue
@@ -1,5 +1,7 @@
 <template>
-  <div>vue</div>
+  <div class="login_container">
+    <div class="login_box">111</div>
+  </div>
 </template>
 
 <script>
@@ -7,4 +9,19 @@ export default {};
 </script>
 
 <style lang="less" scoped>
+.login_container {
+  background-color: #28486e;
+  height: 100%;
+  .login_box {
+    height: 300px;
+    width: 450px;
+    background-color: #fff;
+    border-radius: 3px;
+    position: absolute;
+    left: 50%;
+    top: 50%;
+    transform: translate(-50%, -50%);
+  }
+}
 </style>
+
