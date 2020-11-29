320dda637cc4eee73d0b613a58e942b28cd5d094
zengwenlongstar <1658451483@qq.com>
2020-11-29 08:41:27 +0800

    第三次提交

--- vue-shop/src/router/index.js
+++ vue-shop/src/router/index.js
@@ -1,6 +1,10 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 import Login from '../components/Login.vue'
+import Home from '../components/Home.vue'
+import Welcome from '../components/Welcome.vue'
+import Users from '../components/user/Users.vue'
+
 Vue.use(VueRouter)
 
 const routes = [{
@@ -9,11 +13,22 @@ const routes = [{
 }, {
     path: '/login',
     component: Login
+}, {
+    path: '/home',
+    redirect: '/welcome',
+    component: Home,
+    children: [{ path: '/welcome', component: Welcome }, { path: '/users', component: Users }]
 }]
+
 
 const router = new VueRouter({
     routes
 })
-
+router.beforeEach((to, form, next) => {
+    if (to.path == '/login') return next()
+    const toKen = window.sessionStorage.getItem("token");
+    if (!toKen) return next('/login')
+    next()
+})
 
 export default router
